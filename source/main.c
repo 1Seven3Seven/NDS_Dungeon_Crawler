97e6b487b7267aafcd8226b67991e829c1876e5a
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-05-16 00:32:33 +0930

    Small changes to main to setup stuff

--- source/main.c
+++ source/main.c
@@ -3,13 +3,29 @@
 
 int main(void)
 {
+    // Setting up the top screen for sprites
+    videoSetMode(MODE_0_2D | DISPLAY_BG0_ACTIVE);
+    vramSetBankA(VRAM_A_MAIN_SPRITE);
+    oamInit(&oamMain, SpriteMapping_1D_128, false);
+
+    // Bottom screen for simple text
     consoleDemoInit();
-    iprintf("Hello World!");
+
+    // Other information to track
+    int frame_counter = 0;
+
     while (1)
     {
-        swiWaitForVBlank();
+        consoleClear();
         scanKeys();
-        int pressed = keysDown();
-        if (pressed & KEY_START) break;
+        int keys = keysHeld();
+
+        iprintf("Hello World!\n");
+        iprintf("Frame counter = %d\n", frame_counter);
+
+        frame_counter++;
+        swiWaitForVBlank();
+        oamUpdate(&oamMain);
+        if (keys & KEY_START) break;
     }
 }
