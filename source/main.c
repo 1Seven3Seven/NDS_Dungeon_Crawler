f7e6f3ea6b5a0aad6626a8db54395839e348d36b
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-05-23 02:07:43 +0930

    Still testing code, just a little different. Moved Wifi setup into its own function. Last 10 messages have their first 9 bytes printed. I am attempting to have a square share its position and that be accurately reflected on another device. For some reason the packets do not send unless their beginnings are in a particular format, or at least that is what it seems to me. I feel like I am getting there, but this is getting a bit annoying. Anyways, the other device receives the message if the first four bytes are 48 65 00 00 convert hex to binary. Which is "He\0\0". You can see that "He" is from the string in the second attempt at sending something, but for some reason part of the data array is appended at the end, so the other device prints 48 65 00 00 17 00 00 00 28. The 17 is the identification byte I use, and the 00 00 00 28 is the x position of the square. I know this because I can see it changing on the other device. I do not really want to just throw this beginning as is without figuring out why it does not accept other attempts. But there is something here at least.
    Well, that was a long message.
    I wonder if anyone will end up reading it.
    If you do, well I hope your day is going well, and if it isnt, then good luck.
    - Seven

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #include <dswifi9.h>
 
 // Standard libraries
+#include <netinet/in.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,10 +12,78 @@
 // My libraries
 #include "UI.h"
 
+// #define DATA_LEN sizeof(char) + sizeof(int) * 2
+#define DATA_LEN sizeof(int) * 3
+#define CHARS_CAN_PRINT 9 < DATA_LEN ? 9 : DATA_LEN
+#define POSITION_INDICATOR 0x17
+
+int other_position[2] = {-32, -32};
+
+int frame_counter = 0;
+volatile int frame_save = 0;
+volatile int line_to_print_on = 10;
+volatile int previous_line_to_print_on = 10;
+
 void packet_handler(int packetID, int packetLength)
 {
-    // char data[1024];
+    if (frame_counter != frame_save)
+    {
+        for (int i = 10; i < 20; i++)
+        {
+            UI_DisplayBuffer[i][0] = ' ';
+        }
+
+        frame_save = frame_counter;
+        previous_line_to_print_on = line_to_print_on;
+    }
+
+    char *data = malloc(sizeof(char) * (packetLength + (packetLength % 2)));
+
     // int data_read = Wifi_RxRawReadPacket(packetID, packetLength, (u16 *)data);
+    Wifi_RxRawReadPacket(packetID, packetLength, (u16 *)data);
+
+    UI_DisplayBuffer[line_to_print_on][0] = '>';
+    for (int i = 0; i < (CHARS_CAN_PRINT < packetLength ? CHARS_CAN_PRINT : packetLength); i++)
+    {
+        snprintf(UI_DisplayBuffer[line_to_print_on] + i * 3 + 2, 3, "%02x", data[i]);
+    }
+    UI_RemoveNullsFromLine(line_to_print_on);
+
+    line_to_print_on++;
+
+    if (line_to_print_on >= 20)
+    {
+        line_to_print_on = 10;
+    }
+
+    if (data[0] != POSITION_INDICATOR)
+    {
+        goto packet_handler_cleanup;
+    }
+
+packet_handler_cleanup:
+    free(data);
+}
+
+int setup_wifi()
+{
+    if (!Wifi_InitDefault(INIT_ONLY))
+    {
+        UI_PrintToLine(0, "Error initialising Wifi library");
+        return 0;
+    }
+    else
+    {
+        UI_PrintToLine(0, "Wifi library initialised");
+    }
+    // Use channel 1, can be anything between 1 and 13.
+    Wifi_SetChannel(1);
+    // We handle all received packets
+    Wifi_SetPromiscuousMode(1);
+    Wifi_RawSetPacketHandler(packet_handler);
+    Wifi_EnableWifi();
+
+    return 1;
 }
 
 int main(void)
@@ -59,32 +128,10 @@ int main(void)
            false                            // mosaic
     );
 
-    /*******************
-     * Setting up wifi *
-     *******************/
-    // int wifi_works = 0;
+    oamSet(&oamMain, 1, other_position[0], other_position[1], 0, 0, SpriteSize_32x32, SpriteColorFormat_256Color,
+           gfx_green_square, -1, false, false, false, false, false);
 
-    if (!Wifi_InitDefault(INIT_ONLY))
-    {
-        UI_PrintToLine(0, "Error initialising Wifi library");
-        goto skip_rest_wifi_stuff;
-    }
-    else
-    {
-        // wifi_works = 1;
-        UI_PrintToLine(0, "Wifi library initialised");
-    }
-    // Use channel 1, can be anything between 1 and 13.
-    Wifi_SetChannel(1);
-    // We handle all received packets
-    Wifi_SetPromiscuousMode(1);
-    Wifi_RawSetPacketHandler(packet_handler);
-    Wifi_EnableWifi();
-
-skip_rest_wifi_stuff:  // Take a guess
-
-    // Other information to track
-    int frame_counter = 0;
+    int wifi_works = setup_wifi();
 
     while (1)
     {
@@ -114,12 +161,27 @@ skip_rest_wifi_stuff:  // Take a guess
         UI_PrintToLine(3, "%d, %d", my_position[0], my_position[1]);
         oamSetXY(&oamMain, 0, my_position[0], my_position[1]);
 
-        // if (wifi_works)
-        // {
-        //     char data[1024];
+        if (wifi_works)
+        {
+            char data[DATA_LEN + sizeof(Wifi_TxHeader) + 10] = {'\0'};
 
-        //     Wifi_RawTxFrame(/* data_length */ +8, 0x0014, (u16 *)data);
-        // }
+            data[0] = POSITION_INDICATOR;
+            *(int *)(data + sizeof(char)) = htonl(my_position[0]);
+            *(int *)(data + sizeof(char) + sizeof(int)) = htonl(my_position[1]);
+
+            Wifi_RawTxFrame(DATA_LEN + sizeof(Wifi_TxHeader) + 10, 0x0014, (u16 *)data);
+
+            char _temp1[100] = {'\0'};
+            char string[] = "He";
+            char _temp2[100] = {'\0'};
+            Wifi_RawTxFrame(sizeof(string) + 100, 0x0014, (u16 *)string);
+
+            for (int i = 0; i < DATA_LEN; i++)
+            {
+                snprintf(UI_DisplayBuffer[23] + i * 3, 3, "%02x", data[i]);
+            }
+            UI_RemoveNullsFromLine(23);
+        }
 
         frame_counter++;
         UI_PrintDisplayBuffer();
