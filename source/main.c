58bc8db6cf147a3101a26bbbb6fa8bce0156995b
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-05-22 23:50:50 +0930

    Clearing out a bunch of stuff I do not believe I need anymore

--- source/main.c
+++ source/main.c
@@ -71,82 +71,10 @@ int PrintToLine(int line_number, const char *fmt, ...)
     return return_value;
 }
 
-void PrintBuffer(const char *buffer, int buffer_len, int buffer_offset, int start_line, int end_line)
-{
-    int line = start_line;
-    int character = 0;
-    int index = 0;
-
-    for (int i = 0; i < (UI_NUM_CHARS < buffer_len ? UI_NUM_CHARS : buffer_len); i++)
-    {
-        DisplayBuffer[23][i] = buffer[i];
-    }
-
-    while (line < end_line && index < buffer_len)
-    {
-        DisplayBuffer[line][character] = buffer[buffer_offset + index];
-
-        index++;
-        character++;
-        if (character == UI_NUM_CHARS)
-        {
-            character = 0;
-            line++;
-        }
-    }
-}
-
-typedef struct _Packet
-{
-    int packetID;
-    int packetLength;
-    char data[1024];
-} Packet;
-
-Packet CreatePacket(int packetID, int packetLength, const char *data, int n)
-{
-    Packet packet;
-
-    packet.packetID = packetID;
-    packet.packetLength = packetLength;
-    memcpy(packet.data, data, n < 1024 ? n : 1024);
-    packet.data[1023] = '\0';
-
-    return packet;
-}
-
-#define NUM_PACKETS_TO_CAPTURE 64
-Packet buncha_packets[NUM_PACKETS_TO_CAPTURE];
-int packets_collected = 0;
-int line_to_display_on = 8;
-
 void packet_handler(int packetID, int packetLength)
 {
-    if (packets_collected >= NUM_PACKETS_TO_CAPTURE)
-    {
-        return;
-    }
-
     char data[1024];
-
     int data_read = Wifi_RxRawReadPacket(packetID, packetLength, (u16 *)data);
-
-    buncha_packets[packets_collected++] = CreatePacket(packetID, packetLength, data, data_read);
-
-    PrintToLine(2, "Collecting packets, %d", packets_collected);
-
-    DisplayBuffer[line_to_display_on + 0][0] = ' ';
-    DisplayBuffer[line_to_display_on + 1][0] = ' ';
-
-    line_to_display_on += 2;
-
-    if (line_to_display_on >= UI_NUM_LINES)
-    {
-        line_to_display_on = 8;
-    }
-
-    PrintToLine(line_to_display_on + 0, ">  ID: %06d, Len: %04D", packetID, packetLength);
-    PrintToLine(line_to_display_on + 1, "> %s", data);
 }
 
 int main(void)
@@ -186,12 +114,7 @@ int main(void)
 skip_rest_wifi_stuff:  // Take a guess
 
     // Other information to track
-    int num_sent = 0;
 
-    PrintToLine(4, "Collecting packets");
-    PrintToLine(5, "Press a to send one");
-
-    // Packet capture/sending loop
     while (1)
     {
         consoleClear();
@@ -202,105 +125,9 @@ skip_rest_wifi_stuff:  // Take a guess
         if (keys_down & KEY_A && wifi_works)
         {
             char data[1024];
-            for (int i = 0; i < 1024; i++)
-            {
-                data[i] = '\0';
-            }
 
-            snprintf(data, 1024, "Hello, World number %d", num_sent);
-
-            int data_length = strlen(data) + 1;
-
-            Wifi_RawTxFrame(data_length + 8, 0x0014, (u16 *)data);
-
-            PrintToLine(4, "Sending:");
-            PrintToLine(5, "%s", data);
-
-            num_sent++;
+            Wifi_RawTxFrame(/* data_length */ +8, 0x0014, (u16 *)data);
         }
-
-        PrintDisplayBuffer();
-
-        swiWaitForVBlank();
-        oamUpdate(&oamMain);
-        if (keys_held & KEY_START) return 0;
-        if (packets_collected == NUM_PACKETS_TO_CAPTURE) break;
-    }
-
-    ResetDisplayBuffer();
-
-    PrintToLine(0, "Displaying packets");
-
-    int packet_index = 0;
-    int view_window_start = 0;
-    int display_type = 0;
-    char *display_type_strings[2] = {"ASCII", "HEX"};
-
-    while (1)
-    {
-        consoleClear();
-        scanKeys();
-        int keys_held = keysHeld();
-        // int keys_down = keysDown();
-        int keys_repeat = keysDownRepeat();
-
-        // Adjusting the packet index
-        if (keys_repeat & KEY_RIGHT)
-        {
-            packet_index++;
-            if (packet_index >= NUM_PACKETS_TO_CAPTURE)
-            {
-                packet_index = 0;
-            }
-        }
-        if (keys_repeat & KEY_LEFT)
-        {
-            packet_index--;
-            if (packet_index < 0)
-            {
-                packet_index = NUM_PACKETS_TO_CAPTURE - 1;
-            }
-        }
-
-        // Adjusting the display type
-        if (keys_repeat & KEY_A)
-        {
-            display_type = 0;
-        }
-        if (keys_repeat & KEY_X)
-        {
-            display_type = 1;
-        }
-
-        PrintToLine(1, "Packet index %d", packet_index);
-        PrintToLine(2, "Display type %s", display_type_strings[display_type]);
-
-        Packet packet = buncha_packets[packet_index];
-
-        PrintToLine(4, ">  ID: %d, Len: %D", packet.packetID, packet.packetLength);
-
-        if (display_type == 0)
-        {
-            for (int i = 10; i < 20; i++)
-            {
-                ClearLine(i);
-            }
-
-            PrintBuffer(packet.data, packet.packetLength, 0, 10, 20);
-        }
-        else if (display_type == 1)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    sprintf(DisplayBuffer[10 + i] + j * 3, "%02X ", packet.data[i * 10 + j]);
-                }
-                RemoveNullsFromLine(i);
-            }
-        }
-
-        PrintToLine(23, "%s", packet.data);
 
         PrintDisplayBuffer();
 
