ec219b42c3508c03b9b3bb5235a31977a0a45e67
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-06-14 21:46:13 +0930

    Removing all the net code

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 // NDS stuff
 #include <nds.h>
 // NDS wifi
-#include <dswifi9.h>
+// #include <dswifi9.h>
 
 // Standard libraries
 #include <netinet/in.h>
@@ -11,139 +11,6 @@
 
 // My libraries
 #include "UI.h"
-
-// #define DATA_LEN sizeof(char) + sizeof(int) * 2
-#define DATA_LEN sizeof(char) * 8 + sizeof(int) * 2
-#define CHARS_CAN_PRINT 10 < DATA_LEN ? 10 : DATA_LEN
-#define POSITION_INDICATOR 0x17
-
-#define HEADER_STR "Hello, World!"
-
-volatile int other_position[2] = {-32, -32};
-
-int frame_counter = 0;
-volatile int frame_save = 0;
-volatile int line_to_print_on = 10;
-volatile int previous_line_to_print_on = 10;
-
-void PrintByteArrayAsHex(const u8 *array, int array_len, int line_start, int line_end)
-{
-    if (line_start < 0)
-    {
-        line_start = 0;
-    }
-    if (line_end >= UI_NUM_LINES)
-    {
-        line_end = UI_NUM_LINES - 1;
-    }
-
-    int line_number = line_start;
-    int hex_number = 0;
-    int next_byte = 0;
-
-    while (line_number <= line_end && next_byte < array_len)
-    {
-        UI_ClearLine(line_number);
-
-        while (hex_number < 8 && next_byte < array_len)
-        {
-            snprintf(UI_DisplayBuffer[line_number] + hex_number * 3, 3, "%02x", array[next_byte]);
-            hex_number++;
-            next_byte++;
-        }
-
-        UI_RemoveNullsFromLine(line_number);
-        line_number++;
-        hex_number = 0;
-    }
-}
-
-void PrintPacket(const char *data, int packet_length, int start)
-{
-    UI_DisplayBuffer[line_to_print_on][0] = '>';
-
-    for (int _i = 0; _i < (CHARS_CAN_PRINT < packet_length ? CHARS_CAN_PRINT : packet_length); _i++)
-    {
-        int i = _i + start;
-
-        if (i >= packet_length)
-        {
-            break;
-        }
-
-        snprintf(UI_DisplayBuffer[line_to_print_on] + (i - 8) * 3 + 2, 3, "%02x", data[i]);
-    }
-    UI_RemoveNullsFromLine(line_to_print_on);
-
-    line_to_print_on++;
-
-    if (line_to_print_on >= 20)
-    {
-        line_to_print_on = 10;
-    }
-}
-
-void packet_handler(int packetID, int packetLength)
-{
-    if (frame_counter != frame_save)
-    {
-        for (int i = 10; i < 20; i++)
-        {
-            UI_DisplayBuffer[i][0] = ' ';
-        }
-
-        frame_save = frame_counter;
-        previous_line_to_print_on = line_to_print_on;
-    }
-
-    char *data = malloc(sizeof(char) * (packetLength + (packetLength % 2)));
-
-    // int data_read = Wifi_RxRawReadPacket(packetID, packetLength, (u16 *)data);
-    Wifi_RxRawReadPacket(packetID, packetLength, (u16 *)data);
-
-    if (strncmp(HEADER_STR, data, 8))
-    {
-        // UI_PrintToLine(5, "no");
-        goto packet_handler_cleanup;
-    }
-
-    PrintPacket(data, packetLength, 8);
-
-    int x = ntohl(*(int *)(data + sizeof(int) * 2));
-    int y = ntohl(*(int *)(data + sizeof(int) * 3));
-
-    other_position[0] = x;
-    other_position[1] = y;
-
-
-    UI_PrintToLine(6, "packet:");
-    UI_PrintToLine(7, "  pos: %04x, %04x", x, y);
-    UI_PrintToLine(8, "  len: %d", packetLength);
-
-packet_handler_cleanup:
-    free(data);
-}
-
-int setup_wifi()
-{
-    if (!Wifi_InitDefault(INIT_ONLY))
-    {
-        UI_PrintToLine(0, "Error initialising Wifi library");
-        return 0;
-    }
-    else
-    {
-        UI_PrintToLine(0, "Wifi library initialised");
-    }
-    // Use channel 1, can be anything between 1 and 13.
-    Wifi_SetChannel(1);
-    // We handle all received packets
-    Wifi_SetPromiscuousMode(1);
-    Wifi_RawSetPacketHandler(packet_handler);
-    Wifi_EnableWifi();
-
-    return 1;
-}
 
 int main(void)
 {
@@ -158,18 +25,16 @@ int main(void)
 
     // A really simple sprite
     u16 *gfx_red_square = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-    u16 *gfx_green_square = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
 
     for (int i = 0; i < 32 * 32 / 2; i++)
     {
         gfx_red_square[i] = 1 | (1 << 8);
-        gfx_green_square[i] = 2 | (2 << 8);
     }
 
     SPRITE_PALETTE[1] = RGB15(31, 0, 0);
-    SPRITE_PALETTE[2] = RGB15(0, 31, 0);
 
     int my_position[2] = {100, 100};
+    int frame_counter = 0;
 
     oamSet(&oamMain,                        // Oam
            0,                               // id
@@ -187,18 +52,13 @@ int main(void)
            false                            // mosaic
     );
 
-    oamSet(&oamMain, 1, other_position[0], other_position[1], 0, 0, SpriteSize_32x32, SpriteColorFormat_256Color,
-           gfx_green_square, -1, false, false, false, false, false);
-
-    int wifi_works = setup_wifi();
-
     while (1)
     {
         consoleClear();
         scanKeys();
         int keys_held = keysHeld();
 
-        UI_PrintToLine(2, "frame_counter = %d", frame_counter);
+        UI_PrintToLine(0, "frame_counter = %d", frame_counter);
 
         if (keys_held & KEY_LEFT)
         {
@@ -217,34 +77,7 @@ int main(void)
             my_position[1] += 1;
         }
 
-        UI_PrintToLine(3, "my pos:    %d, %d", my_position[0], my_position[1]);
-        UI_PrintToLine(4, "other pos: %d, %d", other_position[0], other_position[1]);
-
         oamSetXY(&oamMain, 0, my_position[0], my_position[1]);
-        oamSetXY(&oamMain, 1, other_position[0], other_position[1]);
-
-        if (wifi_works)
-        {
-            // u8 data[DATA_LEN] = {'\0'};
-
-            // data[0] = POSITION_INDICATOR;
-            // data[0] = 'H';
-            // data[1] = 'e';
-            // *(int *)(data + sizeof(int)) = htonl(my_position[0]);
-            // *(int *)(data + sizeof(int) * 2) = htonl(my_position[1]);
-
-            // Wifi_RawTxFrame(DATA_LEN, 0x0014, (u16 *)data);
-
-            char *string2 = calloc(30, sizeof(char));
-            strncpy(string2, HEADER_STR, 8);
-            *(int *)(string2 + sizeof(int) * 2) = htonl(my_position[0]);
-            *(int *)(string2 + sizeof(int) * 3) = htonl(my_position[1]);
-            Wifi_RawTxFrame(30, 0x0014, (u16 *)string2);
-
-            PrintByteArrayAsHex((u8 *)string2, DATA_LEN, 22, UI_NUM_LINES);
-
-            free(string2);
-        }
 
         frame_counter++;
         UI_PrintDisplayBuffer();
